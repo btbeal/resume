44ee5d373612ddd083b093276f81208396f09045
btbeal <btbeal@email.unc.edu>
2022-03-26 16:05:03 -0400

    Initial Commit

---
+++ beal_cv/cv_data.R
@@ -0,0 +1,25 @@
+work <- tribble(
+  ~title, ~unit, ~startMonth, ~startYear, ~endMonth, ~endYear, ~where, ~detail,
+  "Quantitative Scientist", "Flatiron Health", "January", 2021, "Present", NA, "New York, NY", "Leading the Syntech team in the automation and deployment of our quality assessment pipeline",
+  "Quantitative Scientist", "Flatiron Health", "January", 2021, "Present", NA, "New York, NY", "Building QS-specific R packages to help scale the way we pressure test our data ",
+  "Quantitative Scientist", "Flatiron Health", "January", 2021, "Present", NA, "New York, NY", "Collaborating with our research clinicians to conduct research on barriers to receiving life-saving oncology therapies",
+  "Quantitative Scientist", "Flatiron Health", "January", 2021, "Present", NA, "New York, NY", "Thought leader regarding the transportability of US RWE for ex-US use cases",
+  "Post-Doctoral Healthcare Economics Research Fellow", "AbbVie in conjunction with the University of Washington", "June", 2019, "December", 2020, "Seattle, WA", "Led a team of 3 to re-imagine the CHOICE REAdi Tool, working with a CHOICE faculty member and a mentee to close a multi-year project funded by our Corporate Advisory Board’s Health Tech Fund",
+  "Post-Doctoral Healthcare Economics Research Fellow", "AbbVie in conjunction with the University of Washington", "June", 2019, "December", 2020, "Seattle, WA", "Developed economic models to demonstrate the cost-effectiveness of an early-stage gene therapy",
+  "Post-Doctoral Healthcare Economics Research Fellow", "AbbVie in conjunction with the University of Washington", "June", 2019, "December", 2020, "Seattle, WA", "Designed and led a 3-day 'Intro to R' course for incoming CHOICE M.S. and Ph.D. students",
+  "Independent Consultant", "Chiesi USA, Inc.", "October", 2018, "June", 2019, "Raleigh, NC", "Consulted with Chiesi's decision-support team to make use of their marketing database ",
+  "Independent Consultant", "Chiesi USA, Inc.", "October", 2018, "June", 2019, "Raleigh, NC", "Recommended a restructure of FY 2020 marketing strategy by identifying a $600k profit opportunity within their provider outreach program"
+  
+ 
+  )
+
+
+edu <- tribble(
+  ~inst, ~endYear, ~deg, ~where, ~detail,
+  "University of Washington CHOICE Institute", 2020, "Masters, Healthcare Economics and Outcomes Research, GPA 3.7", "Seattle, WA","University of Washington School of Pharmacy Graduate Student Leadership Award",
+  "University of North Carolina Eshelman School of Pharmacy", 2019, "Doctor of Pharmacy, GPA 3.4", "Chapel Hill, NC", "PCAT 93rd Percentile (99th percentile in Quant. Reasoning)",
+  "North Carolina State Univesity", 2015, "Bachelors Structural and Molecular Biochemistry, GPA 3.4", "Raleigh, NC", NA
+  )
+
+
+
