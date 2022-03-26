f0b8b65e24590e449254e5b1e8c00169475201ea
btbeal <btbeal@email.unc.edu>
2022-03-26 17:12:44 -0400

    Updating CV

--- beal_cv/cv_data.R
+++ beal_cv/cv_data.R
@@ -4,9 +4,10 @@ work <- tribble(
   "Quantitative Scientist", "Flatiron Health", "January", 2021, "Present", NA, "New York, NY", "Building QS-specific R packages to help scale the way we pressure test our data ",
   "Quantitative Scientist", "Flatiron Health", "January", 2021, "Present", NA, "New York, NY", "Collaborating with our research clinicians to conduct research on barriers to receiving life-saving oncology therapies",
   "Quantitative Scientist", "Flatiron Health", "January", 2021, "Present", NA, "New York, NY", "Thought leader regarding the transportability of US RWE for ex-US use cases",
-  "Post-Doctoral Healthcare Economics Research Fellow", "AbbVie in conjunction with the University of Washington", "June", 2019, "December", 2020, "Seattle, WA", "Led a team of 3 to re-imagine the CHOICE REAdi Tool, working with a CHOICE faculty member and a mentee to close a multi-year project funded by our Corporate Advisory Board’s Health Tech Fund",
+  "Post-Doctoral Healthcare Economics Research Fellow", "AbbVie in conjunction with the University of Washington", "June", 2019, "December", 2020, "Seattle, WA", "Led a team of 3 to create the CHOICE REAdi Tool, an RShiny app to assist with Real World Evidence assessment",
   "Post-Doctoral Healthcare Economics Research Fellow", "AbbVie in conjunction with the University of Washington", "June", 2019, "December", 2020, "Seattle, WA", "Developed economic models to demonstrate the cost-effectiveness of an early-stage gene therapy",
   "Post-Doctoral Healthcare Economics Research Fellow", "AbbVie in conjunction with the University of Washington", "June", 2019, "December", 2020, "Seattle, WA", "Designed and led a 3-day 'Intro to R' course for incoming CHOICE M.S. and Ph.D. students",
+  "Post-Doctoral Healthcare Economics Research Fellow", "AbbVie in conjunction with the University of Washington", "June", 2019, "December", 2020, "Seattle, WA", "Completed Masters coursework and thesis in healathcare economics, biostatistics, and epidemiology",
   "Independent Consultant", "Chiesi USA, Inc.", "October", 2018, "June", 2019, "Raleigh, NC", "Consulted with Chiesi's decision-support team to make use of their marketing database ",
   "Independent Consultant", "Chiesi USA, Inc.", "October", 2018, "June", 2019, "Raleigh, NC", "Recommended a restructure of FY 2020 marketing strategy by identifying a $600k profit opportunity within their provider outreach program"
   
@@ -22,4 +23,17 @@ edu <- tribble(
   )
 
 
+skills <- tribble(
+  ~area, ~skills,
+  "Programming", "R, RMarkdown, RShiny",
+  "Version Control", "Git",
+  "Editors", "RStudio, IntelliJ"
+)
+
+
+packages <- tribble(
+  ~area, ~accomplishment, ~year, ~where, ~detail,
+  "adheRenceRX", "An opinionated package set on increasing the speed and intuition of with which researchers can conduct medication adherence assessment", "December 2020", "https://btbeal.github.io/adheRenceRX/", "Author and Maintainer",
+  "adheRenceRX", "An opinionated package set on increasing the speed and intuition of with which researchers can conduct medication adherence assessment", "December 2020", "https://btbeal.github.io/adheRenceRX/", "Released to CRAN",
+)
 
